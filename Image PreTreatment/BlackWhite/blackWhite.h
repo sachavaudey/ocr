4ac56ec935b325d5effbad3680114b01fe916676
Arthur Wambst <wambst.arthur2@gmail.com>
2024-09-14 00:13:56 +0200

    sam. 14 sept. 2024 00:13:56 CEST

--- Image PreTreatment/BlackWhite/blackWhite.h
+++ Image PreTreatment/BlackWhite/blackWhite.h
@@ -4,5 +4,7 @@
 
 #ifndef BLACKWHITE_H
 #define BLACKWHITE_H
+#include <SDL2/SDL_surface.h>
+void blackWhite(SDL_Surface *surface);
 
 #endif //BLACKWHITE_H
--- Image PreTreatment/Grayscaling/grayscaling.h
+++ Image PreTreatment/Grayscaling/grayscaling.h
@@ -1,11 +1,11 @@
 //
 // Created by arthur_wambst on 13/09/24.
 //
-#include <SDL2/SDL.h>
+
 
 #ifndef GRAYSCALING_H
 #define GRAYSCALING_H
-
+#include <SDL2/SDL.h>
 void grayscaling(SDL_Surface *surface);
 
 #endif //GRAYSCALING_H
