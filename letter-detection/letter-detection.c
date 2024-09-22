264fa721fc71844c56c2ec172b050bd503a22354
vaudey <sacha.vaudey@epita.fr>
2024-09-22 11:30:24 +0200

    refactor:remake letter ddetection functions

--- letter-detection/letter-detection.c
+++ letter-detection/letter-detection.c
@@ -14,8 +14,10 @@ SDL_Surface* load_image(char *path) {
     return image;
 }
 
-// Function which gets the RGB value of a specific pixel
+
+//get the color of a specific pixel
 void get_pixel_rgb(SDL_Surface *surface, int x, int y, Uint8 *r, Uint8 *g, Uint8 *b) {
+    
     if (x >= 0 && x < surface->w && y >= 0 && y < surface->h) {
         Uint32 pixel = ((Uint32*)surface->pixels)[(y * surface->w) + x];
         SDL_GetRGB(pixel, surface->format, r, g, b);
@@ -24,7 +26,47 @@ void get_pixel_rgb(SDL_Surface *surface, int x, int y, Uint8 *r, Uint8 *g, Uint8
     }
 }
 
-// Function which saves an image given in parameter (in BMP format)
+
+//set a specific color to a specifi pixel
+void set_pixel_rgb(SDL_Surface *surface, int x, int y, Uint8 r, Uint8 g, Uint8 b) {
+    
+    if (x >= 0 && x < surface->w && y >= 0 && y < surface->h) {
+        ((Uint32*)surface->pixels)[(y * surface->w) + x] = SDL_MapRGB(surface->format, r, g, b);
+    } else {
+        errx(1, "Error: Pixel coordinates (%d, %d) are out of bounds\n", x, y);
+    }
+}
+
+// TEMP function (NOT TESTED YET)
+void auxilary(SDL_Surface *surface){
+    int weight = surface->w;
+    int height = surface->h;
+    Uint8 r,g,b;
+
+    for(int x = 0; x < weight; x++){
+        double pixAverage = 0;
+        for(int y = 0; y < height; y++){
+            get_pixel_rgb(surface, x, y, &r, &g, &b);
+            printf("RGB : (%d, %d, %d)\n", r, g, b);
+            pixAverage += (r+g+b)/3;
+
+            
+        }
+        if (pixAverage/height > 128){
+            for(int y = 0; y < height; y++){
+                set_pixel_rgb(surface, x, y, 124, 252, 0);
+            }
+        }
+
+    }
+}
+
+
+/*
+* Function wich save image according his actual surface version
+* @param *image : pointer to the image to save
+* @param *path : pointer to the path where save image
+*/
 void save_image(SDL_Surface *image, char *path){
     if (SDL_SaveBMP(image, path) != 0) {
         errx(1, "Error: Couldn't save image to %s. SDL_Error: %s\n", path, SDL_GetError());
@@ -33,88 +75,6 @@ void save_image(SDL_Surface *image, char *path){
     }
 }
 
-//Sub function wich return the row number of the firsst average >128 white pixel
-int detect_column(SDL_Surface *image, int isBlack, int x){
-    int i;
-    int average = 0;
-    Uint8 r, g, b;
-    for(i = x; i < image->h; i++){
-        get_pixel_rgb(image, x, i, &r, &g, &b);
-        average += (r + g + b);
-    }
-    average /= image->h;
-    printf("Average: %d\n", average);
-    if(isBlack == 0 && average > 128){
-        return i;
-    }
-    if(isBlack == 1 && average < 128){
-        return i;
-    }
-    
-    return -1;
-}
-
-int detect_row(SDL_Surface *image, int isBlack, int y){
-    int i;
-    int average = 0;
-    Uint8 r, g, b;
-    for(i = y; i < image->w; i++){
-        get_pixel_rgb(image, i, y, &r, &g, &b);
-        average += (r + g + b);
-    }
-    average /= image->w;
-    printf("AVERAGE: %d\n", average);
-    if(isBlack == 0 && average > 128){
-        return i;
-    }
-    if(isBlack == 1 && average < 128){
-        return i;
-    }
-    
-    return -1;
-}
-
-// Function which processes the image and detects the letter
-void detect_letter(SDL_Surface *image){
-    int colStart = 0;
-    int colEnd = 0;
-    int rowStart = 0;
-    int rowEnd = 0;
-    int count = 0;
-
-    while(colStart < image->w){
-        colStart = detect_column(image, 0, colStart);
-        if(colStart == -1) {
-            colStart = 0;
-            continue;
-        }
-
-        colEnd = detect_column(image, 1, colStart);
-        if(colEnd == -1) errx(1, "Error: Couldn't detect the end of the column :(\n");
-
-        while(rowStart < image->h){
-            rowStart = detect_row(image, 0, rowStart);
-            if(rowStart == -1) {
-                colStart = 0;
-                continue;
-            }
-
-            rowEnd = detect_row(image, 1, rowStart);
-            if(rowEnd == -1) errx(1, "Error: Couldn't detect the end of the row :(\n");
-
-            SDL_Surface *letter = SDL_CreateRGBSurface(0, colEnd - colStart, rowEnd - rowStart, 32, 0, 0, 0, 0);
-            SDL_Rect rect = {colStart, rowStart, colEnd - colStart, rowEnd - rowStart};
-            SDL_BlitSurface(image, &rect, letter, NULL);
-            //The image have to be save in the letter.bmp file according to the count value
-            char path[100];
-            sprintf(path, "letter%d.bmp", count);
-            save_image(letter, path);
-            count++;
-            rowStart = rowEnd;
-        }
-        colStart = colEnd;
-    }
-}
 
 int main(int argc, char *argv[]){
     if(argc != 2) errx(1, "Usage: %s <image.bmp>", argv[0]);
@@ -126,8 +86,8 @@ int main(int argc, char *argv[]){
         SDL_Quit();
         return 1;
     }
-    detect_letter(image);
-    SDL_FreeSurface(image);
+    auxilary(image);
+    save_image(image, "result.bmp");
     SDL_Quit();
     return 0;
 }
