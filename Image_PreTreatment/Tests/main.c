abfe3c044da8ab3c85de7ec17ceb75ced95a9072
Arthur Wambst <wambst.arthur2@gmail.com>
2024-10-28 13:34:18 +0100

    fix: fixed global Tests folder

--- Image_PreTreatment/Tests/main.c
+++ Image_PreTreatment/Tests/main.c
@@ -1,38 +1,38 @@
-#include "BlackWhite/blackWhite.h"
-#include "Grayscaling/grayscaling.h"
-#include "NoiseReduction/noisereduction.h"
-#include "Tools/tools.h"
-#include <err.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-
+#include "main.h"
+#include <string.h>
 
 //Main file of the pre-treatment part of the OCR project
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        errx(EXIT_FAILURE, "Invalid number of arguments!\n./pre-treatment <input_image_path> <output_result_path>\n");
+        errx(EXIT_FAILURE, "Invalid number of arguments!\n./pre-treatment <all/rotate/blackwhite/grayscale/> <input_image_path>\n");
     }
 
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         errx(EXIT_FAILURE, "SDL_Init failed: %s\n", SDL_GetError());
     }
 
-    SDL_Surface *surface = load_image(argv[1]);
+    SDL_Surface *surface = load_image(argv[2]);
     if (!surface) {
         errx(EXIT_FAILURE, "Error while loading image: %s\n", IMG_GetError());
     }
 
-    grayscaling(surface);
-    printf("grayscaled\n");
+	if (!strcmp(argv[1],"rotate"))
+		rotate(surface, 20);
 
-    blackWhite(surface);
-    printf("black and white\n");
+	if (!strcmp(argv[1],"grayscale") || !strcmp(argv[1],"all"))
+		grayscaling(surface);
 
-    noise_reduction(surface, 100);
+	if (!strcmp(argv[1],"blackwhite") || !strcmp(argv[1],"all"))
+		blackWhite(surface);
+   
+	if (!strcmp(argv[1],"grayscale") || !strcmp(argv[1],"all"))
+	{noise_reduction(surface, 100);
     printf("noise reduction\n");
+	}
+    
 
-    save_image(surface, argv[2]);
+	save_image(surface, "result.png");//argv[2]);
     printf("image saved\n");
 
     SDL_FreeSurface(surface);
---
+++ Image_PreTreatment/Tests/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <string.h>
+#include "../BlackWhite/blackWhite.h"
+#include "../Grayscaling/grayscaling.h"
+#include "../NoiseReduction/noisereduction.h"
+#include "../Rotation/rotation.h"
+#include "../Tools/tools.h"
+
+int main(int argc, char *argv[]);
+
+#endif
+
