d8ca2e6606dbc5415314cfbf9bf75d2afedfaeb6
Arthur Wambst <wambst.arthur2@gmail.com>
2024-12-05 17:05:26 +0100

    fix: fixed lines

--- image_pre_treatment/rotation/rotation.c
+++ image_pre_treatment/rotation/rotation.c
@@ -148,7 +148,8 @@ void rotate(SDL_Surface** surface_param, int angle)
 		
 		if (pixelDepartNb == NOT_IN_IMAGE) 
 		{
-			rotated_pixels[pixelNb] = SDL_MapRGBA(new_surface->format, 255, 255, 255, 255);
+			rotated_pixels[pixelNb] = SDL_MapRGBA(new_surface->format,
+					255, 255, 255, 255);
 		}
 		else
 		{	
