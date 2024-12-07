671a0ebdef7a1af81bf279ac27b5ad748f7c4574
Arthur Wambst <wambst.arthur2@gmail.com>
2024-12-07 12:55:28 +0100

    added small fix to predict.c and cols

--- detection/src/upgrade.c
+++ detection/src/upgrade.c
@@ -7,9 +7,13 @@
  */
 SDL_Surface *enlarge_image(SDL_Surface *surface)
 {
-    SDL_Surface *enlarged = SDL_CreateRGBSurface(0, surface->w * ENLARGEMENT_FACTOR, surface->h * ENLARGEMENT_FACTOR, 32, 0, 0, 0, 0);
+    SDL_Surface *enlarged = SDL_CreateRGBSurface(0,
+            surface->w * ENLARGEMENT_FACTOR,
+            surface->h * ENLARGEMENT_FACTOR,
+            32, 0, 0, 0, 0);
     
-    if (!enlarged) {
+    if (!enlarged)
+    {
         return NULL;
     }
 
@@ -35,11 +39,17 @@ SDL_Surface *enlarge_image(SDL_Surface *surface)
             {
                 for (int dx = 0; dx < ENLARGEMENT_FACTOR; dx++)
                 {
-                    int pos = ((y * ENLARGEMENT_FACTOR + dy) * (enlarged->pitch / 4)) + (x * ENLARGEMENT_FACTOR + dx);
-                    if (pos >= (enlarged->w * enlarged->h)) {
+                    int pos = 
+                        ((y * ENLARGEMENT_FACTOR + dy) *
+                        (enlarged->pitch / 4)) +
+                        (x * ENLARGEMENT_FACTOR + dx);
+                    
+                    if (pos >= (enlarged->w * enlarged->h))
+                    {
                         fprintf(stderr, "Error: Out of bounds\n");
                         continue;
                     }
+
                     enlargedPixels[pos] = mappedPixel;
                 }
             }
@@ -58,7 +68,10 @@ SDL_Surface *enlarge_image(SDL_Surface *surface)
  */
 SDL_Surface *reduce_image(SDL_Surface *surface)
 {
-    SDL_Surface *reduced = SDL_CreateRGBSurface(0, surface->w / ENLARGEMENT_FACTOR, surface->h / ENLARGEMENT_FACTOR, 32, 0, 0, 0, 0);
+    SDL_Surface *reduced = SDL_CreateRGBSurface(0,
+            surface->w / ENLARGEMENT_FACTOR,
+            surface->h / ENLARGEMENT_FACTOR,
+            32, 0, 0, 0, 0);
     
     if (!reduced) {
         return NULL;
@@ -76,7 +89,11 @@ SDL_Surface *reduce_image(SDL_Surface *surface)
         for (int x = 0; x < reduced->w; x++)
         {
             Uint32 *pixels = (Uint32*)surface->pixels;
-            Uint32 pixel = pixels[(y * ENLARGEMENT_FACTOR) * (surface->pitch / 4) + (x * ENLARGEMENT_FACTOR)];
+            Uint32 pixel =
+                pixels[(y * ENLARGEMENT_FACTOR) *
+                (surface->pitch / 4) +
+                (x * ENLARGEMENT_FACTOR)];
+            
             SDL_GetRGBA(pixel, surface->format, &r, &g, &b, &a);
 
             Uint32 mappedPixel = SDL_MapRGBA(reduced->format, r, g, b, a);
